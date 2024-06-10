d084ad8deef5001add3b8c4280fc4f34e06d89f6
Jesper Lillie <jesper.lillie@gmail.com>
2024-06-10 22:33:53 +1000

    Remove backdrop blur on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Intro() {
 
   return (
     <main className="flex-1 flex flex-col bg-[url('/bg/main.jpg')] bg-center bg-cover">
-      <div className="flex-1 flex flex-col self-stretch justify-center items-center gap-4 backdrop-blur-sm backdrop-brightness-50">
+      <div className="flex-1 flex flex-col self-stretch justify-center items-center gap-4 backdrop-brightness-50">
         <FatLink href={sesh}>Adventure</FatLink>
         <FatLink href="/about">About</FatLink>
       </div>
